98612f8ffae593c81d306eebd8da9afd599323b4
radwanyhassan75 <radwanyhassan75@gmail.com>
2025-09-04 23:56:50 +0100

    kkk

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,3 +1,7 @@
-
+<script src="public/layout.js"></script>
+    <script type="module" src="public/auth.js"></script>
 
     
+    <script type="module" src="auth.js"></script>
+    <script src="layout.js"></script>
+    <link rel="stylesheet" href="layout.css"></link>
