01f0a7d913f65ff6a981facb0c43b032d76f03e8
radwanyhassan75 <radwanyhassan75@gmail.com>
2025-09-05 13:38:32 +0100

    SEO

--- public/dashboard.js
+++
@@ -1,7 +0,0 @@
-<script src="public/layout.js"></script>
-    <script type="module" src="public/auth.js"></script>
-
-    
-    <script type="module" src="auth.js"></script>
-    <script src="layout.js"></script>
-    <link rel="stylesheet" href="layout.css"></link>
